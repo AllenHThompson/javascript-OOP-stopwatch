e7ff2edd9a35ffee1d2f3dc6ac1601166e184290
Allen H. Thompson <allenhthompson1@gmail.com>
2016-08-31 18:27:33 -0400

    Working on start method.

--- stopwatch.js
+++ stopwatch.js
@@ -1,35 +1,45 @@
 $(document).ready(function () {
 
+     // $('#the-button').click(function(){
+     //      $('#the-button').text('Stop');
+     //
+     //      this.start();
+     //
+     // });
+     //
+     // $('#clear-button').click(function(){
+     //      $('#the-button').text('Start');
+     // });
+     //
+     //
+     // $('#timer').text(this.startTime);
 
-     $('#start-button').click(function(){
-
-          $('#display').text('Start');
 
 
-     });
 
-     $('#stop-button').click(function(){
+     function Stopwatch() {
+          this.startTime = 0;
+          this.stopTime = 0;
+          this.totalElasped = 0;
+          this.isStarted = false;
 
-          $('#display').text('Stop');
+          this.oneHour = 1000 * 60 * 60;
+          this.oneMin = 1000 * 60;
+          this.oneSecond = 1000;
 
+     }
 
-     });
+     Stopwatch.prototype.start = function() {
+          this.startTime = new Date().getTime();
+     };
+
+     Stopwatch.prototype.stop = function() {
+
+     };
+
+     Stopwatch.prototype.reset = function() {
+
+     };
 
 
 });
-
-function Stopwatch() {
-
-}
-
-Stopwatch.prototype.start = function() {
-
-};
-
-Stopwatch.prototype.stop = function() {
-
-};
-
-Stopwatch.prototype.reset = function() {
-
-};
