c0eafb5481637464f71dee29ebd5c39ee065a42b
Allen H. Thompson <allenhthompson1@gmail.com>
2016-08-31 15:57:03 -0400

    Initial commit.

---
+++ stopwatch.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+
+     $('#start-button').click(function(){
+
+          $('#display').text('Start');
+
+
+     });
+
+     $('#stop-button').click(function(){
+
+          $('#display').text('Stop');
+
+
+     });
+
+
+});
+
+function Stopwatch() {
+
+}
+
+Stopwatch.prototype.start = function() {
+
+};
+
+Stopwatch.prototype.stop = function() {
+
+};
+
+Stopwatch.prototype.reset = function() {
+
+};
