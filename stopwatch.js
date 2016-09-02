8c07dc311bb0c5e4ae72c1a4ba3fc4f3f2f73d56
Allen H. Thompson <allenhthompson1@gmail.com>
2016-09-01 22:52:40 -0400

    Refactoring constructor methods.

--- stopwatch.js
+++ stopwatch.js
@@ -1,45 +1,90 @@
-$(document).ready(function () {
+var Stopwatch = function() {
+		var	startAt	= 0;
+		var	lapTime	= 0;
 
-     // $('#the-button').click(function(){
-     //      $('#the-button').text('Stop');
-     //
-     //      this.start();
-     //
-     // });
-     //
-     // $('#clear-button').click(function(){
-     //      $('#the-button').text('Start');
-     // });
-     //
-     //
-     // $('#timer').text(this.startTime);
+		var	now	= function() {
+				return (new Date()).getTime();
+			};
 
-
-
-
-     function Stopwatch() {
-          this.startTime = 0;
-          this.stopTime = 0;
-          this.totalElasped = 0;
-          this.isStarted = false;
-
-          this.oneHour = 1000 * 60 * 60;
-          this.oneMin = 1000 * 60;
-          this.oneSecond = 1000;
-
-     }
-
-     Stopwatch.prototype.start = function() {
-          this.startTime = new Date().getTime();
-     };
-
-     Stopwatch.prototype.stop = function() {
-
-     };
-
-     Stopwatch.prototype.reset = function() {
-
-     };
+		this.start = function() {
+				//startAt	= startAt ? startAt : now();
+                    if (startAt) {
+                        startAt = startAt;
+                    } else {
+                        startAt = now();
+                    }
+			};
 
 
-});
+		this.stop = function() {
+				// lapTime	= startAt ? lapTime + now() - startAt : lapTime;
+
+                    if (lapTime) {
+                         lapTime = lapTime + now() - startAt
+                    } else {
+                         lapTime = lapTime
+                    }
+				startAt = 0;
+			};
+
+		this.reset = function() {
+				lapTime = 0;
+                    startAt = 0;
+			};
+
+		this.time = function() {
+				return lapTime + (startAt ? now() - startAt : 0);
+			};
+	};
+
+var $time;
+var clocktimer;
+
+function pad(num, size) {
+	var s = "0000" + num;
+	return s.substr(s.length - size);
+}
+
+function formatTime(time) {
+	var h = m = s = ms = 0;
+	var newTime = '';
+
+	h = Math.floor( time / (60 * 60 * 1000) );
+	time = time % (60 * 60 * 1000);
+	m = Math.floor( time / (60 * 1000) );
+	time = time % (60 * 1000);
+	s = Math.floor( time / 1000 );
+	ms = time % 1000;
+
+	newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+	return newTime;
+}
+
+function show() {
+	$time = document.getElementById('time');
+	update();
+}
+
+function update() {
+	$time.innerHTML = formatTime(x.time());
+}
+
+function start() {
+	clocktimer = setInterval("update()", 1);
+	//x.start();
+	Stopwatch.start();
+}
+
+function stop() {
+	//x.stop();
+	Stopwatch.stop();
+
+	clearInterval(clocktimer);
+}
+
+function reset() {
+	stop();
+	//x.reset();
+	Stopwatch.reset();
+	update();
+}
